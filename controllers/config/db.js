2ee8cafe73fd02cf219927eceb174d185d8ec5ae
unknown <Administrador@COBOGJLRODRI.ifxcorp.com>
2019-06-21 13:04:54 -0500

    clase 30 configurando sequelize

---
+++ controllers/config/db.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize('uptasknode', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: '3306',
+    define: {
+        timestamps: false
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      }
+});
